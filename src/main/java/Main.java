81b673d19b95fb876d7bc141a37f22e42b059099
romainsilvy <56515213+romainsilvy@users.noreply.github.com>
2021-10-08 15:50:24 +0200

    manage args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,14 +11,50 @@ import com.google.gson.*;
 
 public class Main {
 
+    public static Request apiRequest(String strToFind) {
+        System.out.println("searching " + strToFind + ".....\n\n");
+        Request request = new Request.Builder()
+                .url("\n" + ("https://api.themoviedb.org/3/search/multi?api_key=8ac8920a6691cfb0e988250f77cb8e67&query=" + strToFind))
+                .get()
+                .build();
+        return request;
+    }
+
+    public static SearchResponse parseJson(Request request) {
+        SearchResponse request1 = null;
+        OkHttpClient client = new OkHttpClient();
+        try{
+            Gson gson = new GsonBuilder().create();
+            Response response = client.newCall(request).execute();
+            request1 = gson.fromJson(response.body().string(), SearchResponse.class);
+            return request1;
+
+        }catch (Exception e){
+            System.out.println("Error: "+e);
+        }
+        return request1;
+    }
+
+    public static void search(Scanner scanner2) {
+        System.out.println("entrez le nom du film que vous voulez rechercher");
+        String strToFind = scanner2.nextLine();
+        Request request = apiRequest(strToFind);
+        SearchResponse request1 = parseJson(request);
+        request1.printAll();
+    }
+
+    
     public static void main(String[] args) {
 
-
-
-        System.out.println("                                          \n" +
-                "     )))           +++           (((      \n" +
-                "    (o o)         (o o)         (o o)     \n" +
-                "ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-\n");
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equals("--search")) {
+                    System.out.println("searching " + args[i+1]);
+                } else if (args[i].equals("--detail")) {
+                    System.out.println("detailing " + args[i+1]);
+                }
+            }
+        }
 
         Scanner scanner = new Scanner(System.in);
         Scanner scanner2 = new Scanner(System.in);
@@ -31,10 +67,6 @@ public class Main {
                     " | |\\/| | |  __|   | . ` | | |  | |\n" +
                     " | |  | | | |____  | |\\  | | |__| |\n" +
                     " |_|  |_| |______| |_| \\_|  \\____/");
-            System.out.println("                                          \n" +
-                    "     )))           +++           (((      \n" +
-                    "    (o o)         (o o)         (o o)     \n" +
-                    "ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-\n");
             System.out.println("[1] Recherche ");
             System.out.println("[2] Detail");
             System.out.println("[3] Fermer l'application");
@@ -43,47 +75,7 @@ public class Main {
 
             switch (choix) {
                 case 1:
-                    System.out.println("entrez le nom du film que vous voulez rechercher");
-                    String strToFind = scanner2.nextLine();
-                    System.out.println("searching " + strToFind + ".....\n\n");
-
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder()
-                            .url("\n" + ("https://api.themoviedb.org/3/search/multi?api_key=8ac8920a6691cfb0e988250f77cb8e67&query=" + strToFind))
-                            .get()
-                            .build();
-
-                    try{
-                        Gson gson = new GsonBuilder().create();
-                        Response response = client.newCall(request).execute();
-                        SearchResponse request1 = gson.fromJson(response.body().string(), SearchResponse.class);
-
-                        int rangeOnTab = 15;
-                        if (request1.results.length < 15) {
-                            rangeOnTab = request1.results.length;
-                        }
-
-                        for (int i = 0; i < rangeOnTab; i++) {
-                            switch (request1.results[i].media_type) {
-                                case "tv":
-                                    System.out.println("\u001B[32mL'ID du media est " + request1.results[i].id);
-                                    System.out.println("Le nom du media est " + request1.results[i].name);
-                                    break;
-                                case "movie":
-                                    System.out.println("\u001B[34mL'ID du media est " + request1.results[i].id);
-                                    System.out.println("Le nom du media est " + request1.results[i].title);
-                                    break;
-                            }
-                            System.out.println("le type est " + request1.results[i].media_type + "\n\n\u001B[0m");
-                        }
-                    }catch (Exception e){
-                        System.out.println("Error: "+e);
-                        return;
-                    }
-
-
-
-
+                    search(scanner2);
                     break;
                 case 2:
                     System.out.println("entrez l'id du film sur lequel vous voulez des informations");
@@ -99,12 +91,12 @@ public class Main {
                     try{
                         Gson gson = new GsonBuilder().create();
                         Response response = client2.newCall(request2).execute();
-                        Movie request1 = gson.fromJson(response.body().string(), Movie.class);
-                        System.out.println("L'ID du media est " + request1.id);
-                        System.out.println("Le nom du media est " + request1.title);
-                        System.out.println("La description du media est " + request1.overview);
-                        System.out.println("La moyenne des votes du media est " + request1.vote_average);
-                        System.out.println("Le nombre de votes du media est " + request1.vote_count);
+                        Movie request11 = gson.fromJson(response.body().string(), Movie.class);
+                        System.out.println("L'ID du media est " + request11.id);
+                        System.out.println("Le nom du media est " + request11.title);
+                        System.out.println("La description du media est " + request11.overview);
+                        System.out.println("La moyenne des votes du media est " + request11.vote_average);
+                        System.out.println("Le nombre de votes du media est " + request11.vote_count);
 
 
 
--- src/main/java/SearchResponse.java
+++ src/main/java/SearchResponse.java
@@ -1,4 +1,26 @@
 public class SearchResponse {
     int page;
     Media[] results;
+
+
+    void printAll() {
+        int rangeOnTab = 15;
+        if (this.results.length < 15) {
+            rangeOnTab = this.results.length;
+        }
+
+        for (int i = 0; i < rangeOnTab; i++) {
+            switch (this.results[i].media_type) {
+                case "tv":
+                    System.out.println("\u001B[32mL'ID du media est " + this.results[i].id);
+                    System.out.println("Le nom du media est " + this.results[i].name);
+                    break;
+                case "movie":
+                    System.out.println("\u001B[34mL'ID du media est " + this.results[i].id);
+                    System.out.println("Le nom du media est " + this.results[i].title);
+                    break;
+            }
+            System.out.println("le type est " + this.results[i].media_type + "\n\n\u001B[0m");
+        }
+    }
 }
